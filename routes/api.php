3040b7c387acb43b9ffb16931e0ec6abd4f70bc0
Marlon Meneses <marlon-armando@hotmail.com>
2024-07-23 16:53:10 -0500

    Created Model -a

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -16,4 +17,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::apiResource('/products', 'App\Http\Controllers\ProductController');
+
+Route::group(['prefix'=>'products'], function(){
+    Route::apiResource('/{product}/reviews', 'App\Http\Controllers\ReviewController');
 });
