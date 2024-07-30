003df34f37afb34f55f6438e6e96132ca32bb1d4
Marlon Meneses <marlon-armando@hotmail.com>
2024-07-30 15:22:49 -0500

    Seeding part for Product and Review

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 
+use App\Model\Product;
+
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -14,5 +16,9 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        \App\Models\Model\Product::factory(50)->create();
+        \App\Models\Model\Review::factory(300)->create();
+        //factory(App\Model\Product::class, 50)->create();
+        //factory(App\Model\Review::class, 300)->create();
     }
 }
